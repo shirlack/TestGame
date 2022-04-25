e3897ee9de15939d74f440c73ceb26c2db02c655
KIRILL MENZHULOV <Cherhirscat@gmail.com>
2022-04-25 18:26:19 +0300

    Изменен принцип считывания клавишь

--- TestGame/Program.cs
+++ TestGame/Program.cs
@@ -7,59 +7,65 @@ using System.Threading.Tasks;
 
 namespace TestGame {
     class Program
-{
-    static void Main(string[] args)
     {
-        static void StartGame() //Вся программа работает в функции StartGame,в будущем это позволило бы создать меню игры , производить какие то надстройки перед запуском
-        {   
-            Tank Player = new Tank(200, 35, 50, "Игрок");   //Конструктор класса имеет три входных параметра при создании Здоровье,Броню,Урон и Имя объекта
-            Tank Computer = new Tank(300, 15, 60, "Компьютер");
+        enum PlayerAction
+        {
+            PlayerShot = 1,
+            PlayerRepair = 2,
+            PlayerBuyAmmo = 3
+        };
 
-            int Action;     //Создаются дополнительные переменные что бы обрабатывать действия Игрока и Случайные действия Компьютера
-            int ComputerAction;
-            Random rng = new Random(); 
+        static void Main(string[] args)
+        {
+
+            static void StartGame() //Вся программа работает в функции StartGame,в будущем это позволило бы создать меню игры , производить какие то надстройки перед запуском
+            {
+                Tank Player = new Tank(200, 35, 50, "Игрок");   //Конструктор класса имеет три входных параметра при создании Здоровье,Броню,Урон и Имя объекта
+                Tank Computer = new Tank(300, 15, 60, "Компьютер");
+
+                ConsoleKeyInfo Action;     //Создаются дополнительные переменные что бы обрабатывать действия Игрока и Случайные действия Компьютера
+                int ComputerAction;
+                Random rng = new Random();
                 while (Player.Healts > 0 && Computer.Healts > 0)
                 {
 
                     Console.WriteLine("Характеристики Игрока. Здоровье: " + Player.Healts + " Броня:" + Player.Armor + " Боекомплект: " + Player.BulletCount);
                     Console.WriteLine("Характеристики Компьютера. Здоровье: " + Computer.Healts + " Броня:" + Computer.Armor + " Боекомплект: " + Computer.BulletCount);
-                    Console.WriteLine("Выберете действие:" + "\n" + "1.Выстрел " + "\n" + "2.Поднять Броню" + "\n" + "3.Купить Боекомплект");
+                    Console.WriteLine("Выберете действие:" + "\n" + "1.Выстрел " + "\n" + "2.Поднять Броню" + "\n" + "3.Купить Боекомплект\n");
                     //Простой интерфейс с выводом текущих характерист объекта + возможные действия пользователя
 
 
-                    Action = Convert.ToInt32(Console.ReadLine());
+                    Action = Console.ReadKey(true);
                     ComputerAction = rng.Next(1, 3);
 
-                    switch (Action)     //В данном свиче обрабатываются входные параметры и происходит действие согласно выбору пользователя
+                    if (Action.Key == ConsoleKey.D1 || Action.Key == ConsoleKey.NumPad1)//даный метод позволяет принимать клавиши с NumPada и с цифр над клавиатурой
                     {
-                        case 1:
-                            if (Player.BulletCount >= 1) //При выборе Выстрела происходит проврка на наличие потронов
-                            {                            //Если потронов нет, Игрока уведомляют о необходимости их купить и он пропускает свой ход
-                                Player.Shot(Computer);  //если Введено значение которого нет в списке , то предлагается выбрать заново
-                                ComputerMotion();
-                            }
-                            else
-                            {
-                                Console.WriteLine("Купите Боекомплект что бы стрелять");
-                                ComputerMotion();
-                            }
-                            break;
-
-                        case 2:
-                            Player.getArmor();
+                        if (Player.BulletCount >= 1) //При выборе Выстрела происходит проврка на наличие потронов
+                        {                            //Если потронов нет, Игрока уведомляют о необходимости их купить и он пропускает свой ход
+                            Player.Shot(Computer);  //если Введено значение которого нет в списке , то предлагается выбрать заново
                             ComputerMotion();
-                            break;
-
-                        case 3:
-                            Player.Reloud();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Купите Боекомплект что бы стрелять");
                             ComputerMotion();
-                            break;
-
-                        default:
-                            Console.WriteLine("Введенно не верное значение");
-
-                            break;
+                        }                      
                     }
+                    else if (Action.Key == ConsoleKey.NumPad2 || Action.Key == ConsoleKey.D2)
+                    {
+                        Player.getArmor();
+                        ComputerMotion();
+                        
+                    }
+                    else if (Action.Key == ConsoleKey.NumPad3 || Action.Key == ConsoleKey.D3)
+                    {
+                        Player.Reloud();
+                        ComputerMotion();
+                        
+                    }
+                    else {Console.WriteLine("Введенно не верное значение"); }
+                        
+                    
                     void ComputerMotion()               //Функция обработки дейсвий компьютера
                     {if (Computer.Healts == Computer.Max_Healts && Computer.BulletCount >= 1)//Проверка на максимально колличиство здоровья
                         {                                                                   //что бы компьтер не пытался лечится с полным здоровьем 
